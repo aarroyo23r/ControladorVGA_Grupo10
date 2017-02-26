7a573286437b99b7634de6a291749d74c82bb76d
aarroyo23r <aarroyo23r@gmail.com>
2017-02-26 10:21:07 -0600

    Correcciones

--- GeneradosDatos.v
+++ GeneradosDatos.v
@@ -7,9 +7,9 @@ module Generador_datos
     input wire [9:0] pixel_x, pixel_y, //posición pixel actual
     output reg [2:0] rgb_text   // bit de color a VGA
  );
- 
+
  //variables internas de conexió
- 
+
  wire [10:0] rom_addr; //dirección de memoria completa
  wire [6:0] char_addr; //  bits mas significativos de dirreción de memoria
  wire [3:0] row_addr; // bit menos significativos de memoria, para variar filas
@@ -20,14 +20,14 @@ module Generador_datos
  wire [4:0] bajos_x;
  reg [1:0] selecreg;
  reg [1:0] letra;
- 
+
 //body
 assign bit_addr =pixel_x[2:0];
 assign bit5_y = pixel_y[4];
 assign row_addr= pixel_y[3:0]; //4 bits menos significatvos de y
 assign bajos_x = pixel_x[4:0]; // menos significativos de x;
 
-    always @(pixel_x or posedge clk)
+    always @(pixel_x or posedge clk)            // No se pueden mezclar partes conbinacionales y secuenciales en la lista de sensibilidad
     begin
         if (pixel_x < 10'b0000100000)         //Análisis de las filas
             letra = 2'b11;                     // si pixel_x es menor que 8; le asigna la letra E
@@ -38,32 +38,29 @@ assign bajos_x = pixel_x[4:0]; // menos significativos de x;
         else begin
             letra = 2'b00;
             end
-     end     
+     end
 
-    always @ (bit5_y or letra or posedge clk)
-    begin                                       //Análisis por columnas con el 5to bit de pixel_y
-       case(bit5_y)
-       1'b1:selecreg <= 2'b00;
-       1'b0:selecreg <= letra;   
+    always @ (bit5_y or letra or posedge clk)     // No se pueden mezclar partes conbinacionales y secuenciales en la lista de sensibilidad, creo que tiene que escribirlo algo asi (posedge bit5_y, posedge letra, posedge clk)
+    begin                                         // asi cada vez que bit5_y o letra pasen de 0 a 1 entra en el always o cuando hay un flanco de reloj
+       case(bit5_y)                        //Análisis por columnas con el 5to bit de pixel_y
+       1'b1:selecreg <= 2'b00;           //No se bien la sintaxis del case pero me parece que esta combinando variables de distinto numero de bits
+       1'b0:selecreg <= letra;
        endcase
     end
- 
+
 //Registros que almacenan direccionens
 Registros register_unit
        (.clk(clk), .selec(selecreg), .direc(char_addr));
-        
+
 assign rom_addr ={char_addr, row_addr}; //concatena direcciones de registros y filas
 
 //FONT ROM
 Font_rom font_unit //modulo que crea las letras en memoria
    (.clk(clk), .dir(rom_addr), .data(font_word));
 
-assign font_bit =font_word [~bit_addr];              
+assign font_bit =font_word [~bit_addr];
  //rgb multiplexor
 Color color_unir // modulo que determina si pasa o no rgb
-    (.clk(clk), .switch(), .rgb(rgb_text), .bit_let(font_bit), .video_on(video_on));
+    (.clk(clk), .switch(), .rgb(rgb_text), .bit_let(font_bit), .video_on(video_on)); // Aqui el switch no tiene nada asignado
 
 endmodule //
-
-
-
--- Registros.v
+++ Registros.v
@@ -2,18 +2,18 @@
 
 module Registros(
     input wire clk,
-    input wire [1:0] selec,
+    input wire [1:0] selec,  //Aqui corregi el numero de bits, tenia 3 bits y solo son necesarios 2
     output reg [6:0] direc //más significativo de direcciones
     );
-reg [2:0] selec_reg;
+reg [1:0] selec_reg;
 
 always @(posedge clk)
      selec_reg <= selec;
 always @*
    case(selec_reg)
-           3'b00: direc = 7'b0000000;
-           3'b01: direc = 7'b1000101;
-           3'b10: direc = 7'b1000001;
-           3'b11: direc = 7'b1001010;    
+           2'b00: direc = 7'b0000000;  //igual aqui
+           2'b01: direc = 7'b1000101;
+           2'b10: direc = 7'b1000001;
+           2'b11: direc = 7'b1001010;
     endcase
 endmodule
