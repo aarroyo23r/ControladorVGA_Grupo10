771ea379efb3a8ea01d98c18f709218289fb157c
jennifercm <jencamora@hotmail.com>
2017-02-27 00:52:10 -0600

    Update Color.v

--- Color.v
+++ Color.v
@@ -20,19 +20,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Color(
+module Color( //declaración de variables
     input wire clk,
     input wire [7:0] switch,
     output wire [2:0] rgb,
     input wire bit_let,
     input wire video_on
     );
-  
+  //Variable interna
   reg [2:0] rgb_reg;
   
-  always @(posedge clk)
-    if (bit_let)
-        case (switch)
+    always @(posedge clk) //operación se realiza con cada pulso de reloj
+        if (bit_let)  //se encienden los LEDs solo si el bit se encuentra en 1 en memoria
+            case (switch) // combinación de colores seleccionados de acuerdo al switch, solo se puede seleccionar un siwtch a la vez
             8'b00000001: rgb_reg = 3'b001;
             8'b00000010: rgb_reg = 3'b010;
             8'b00000100: rgb_reg = 3'b011;
@@ -44,5 +44,5 @@ module Color(
         endcase
      else 
         rgb_reg <= 0;
-   assign rgb = (video_on) ? rgb_reg : 3'b000;
+    assign rgb = (video_on) ? rgb_reg : 3'b000; //se le asigna a rgb la variable interna solo si la señal de video está encendida
 endmodule
