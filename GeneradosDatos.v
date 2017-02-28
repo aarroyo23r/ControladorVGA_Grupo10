22ea06d894d51d1dc2f4ce9f4174e90bf7934506
Emmanuel Araya Gamboa <emmanuelaraya114@gmail.com>
2017-02-27 22:26:52 -0600

    generador para pruebas
    Se añadio este documento solo para pruebas de testbench del bloque generador de datos (debido a que la versión final no instancia el bloque de font_rom, ni color a este bloque

--- GeneradosDatos.v
+++ GeneradosDatos.v
@@ -4,20 +4,25 @@ module Generador_datos
     (
     input wire clk,
     input wire video_on,
-    input wire [9:0] pixel_x, //posición pixel actual
-    input wire [9:0] pixel_y,
-    output wire [10:0] rom_addr
+    input wire [9:0] pixel_x, pixel_y, //posición pixel actual
+    output wire [2:0] rgb_text,
+    input wire [7:0] switch   // bit de color a VGA
  );
-
+ 
  //variables internas de conexió
-
+ 
+ wire [10:0] rom_addr; //dirección de memoria completa
  wire [6:0] char_addr; //  bits mas significativos de dirreción de memoria
  wire [3:0] row_addr; // bit menos significativos de memoria, para variar filas
  wire [2:0] bit_addr; // señal de control MUX final
  wire [7:0] font_word; // datos de memoria
+ wire font_bit;//variable de salida; antes de color
  wire bit5_y; //5to bit de posicion en y
  reg [1:0] selecreg;
- reg [1:0] letra;   
+ reg [1:0] letra;
+ 
+//body
+assign bit_addr =pixel_x[2:0];
 assign bit5_y = pixel_y[4];
 assign row_addr= pixel_y[3:0]; //4 bits menos significatvos de y
 
@@ -38,18 +43,27 @@ always @ (letra or bit5_y)
        1'b0:selecreg = letra;   
        endcase
     end
-    
 always@(pixel_x)begin    
  if (pixel_x > 10'b0000011000)begin
              selecreg = 2'b00;end
      end
-
-       
+ 
 //Registros que almacenan direccionens
 Registros register_unit
        (.clk(clk), .selec(selecreg), .direc(char_addr));
-
+        
 assign rom_addr ={char_addr, row_addr}; //concatena direcciones de registros y filas
 
+//FONT ROM
+Font_rom font_unit //modulo que crea las letras en memoria
+   (.clk(clk), .dir(rom_addr), .data(font_word));
+
+assign font_bit =font_word [~bit_addr];              
+ //rgb multiplexor
+Color color_unir // modulo que determina si pasa o no rgb
+    (.clk(clk), .switch(switch), .rgb(rgb_text), .bit_let(font_bit), .video_on(video_on));
 
 endmodule //
+
+
+
