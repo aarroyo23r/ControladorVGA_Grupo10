cf36df414aea0884ce47e953cced3d7d7b5220b1
Emmanuel Araya Gamboa <emmanuelaraya114@gmail.com>
2017-02-27 21:23:16 -0600

    Update GeneradosDatos.v

--- GeneradosDatos.v
+++ GeneradosDatos.v
@@ -5,7 +5,7 @@ module Generador_datos
     input wire clk,
     input wire video_on,
     input wire [9:0] pixel_x, //posición pixel actual
-    input wire [9:0] pixel_y,
+    input wire [4:0] pixel_y,
     output wire [10:0] rom_addr
  );
 
