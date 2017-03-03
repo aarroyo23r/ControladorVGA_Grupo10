6968d4fc73badd206b0d1f5b690330a4c33cdb33
jennifercm <jencamora@hotmail.com>
2017-03-03 12:53:17 -0600

    Update GeneradosDatos.v

--- GeneradosDatos.v
+++ GeneradosDatos.v
@@ -25,12 +25,14 @@ assign row_addr= pixel_y[3:0]; //4 bits menos significatvos de y
    begin
        if ((pixel_x < 10'b0000011000) && (pixel_x>10'b0000001111))begin       //Análisis de las filas
             selecreg = 2'b11;end                                               // si pixel_x es menor que 8; le asigna la letra E
-       if ((pixel_x < 10'b0000010000) && (pixel_x>10'b0000000111))begin
+       else if ((pixel_x < 10'b0000010000) && (pixel_x>10'b0000000111))begin
              selecreg = 2'b10;end
-       if (pixel_x < 10'b0000001000)begin
+       else if (pixel_x < 10'b0000001000)begin
              selecreg = 2'b01;end
-       if ((pixel_x > 10'b0000011000) | (pixel_y>10'b0000000111))begin
+       else if ((pixel_x > 10'b0000011000) | (pixel_y>10'b0000000111))begin
                          selecreg = 2'b00;end
+       else begin
+           selecreg = 2'b00;end
   end    
    
     
